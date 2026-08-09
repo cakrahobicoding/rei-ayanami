54c467319685eb31a7d8f57d0c23efce582dfb5f
Ran <cakraganzzx@gmail.com>
2026-08-10 00:59:32 +0700

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,14 @@
-import "next-auth" 
+import { DefaultSession } from "next-auth"
+
 declare module "next-auth" {
- interface session {
-   accessToken?: string
+  interface Session {
+    accessToken?: string
+    user: DefaultSession["user"]
   }
-} 
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
